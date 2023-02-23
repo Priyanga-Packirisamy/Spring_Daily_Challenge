b5fc2bd8a52c3f728068b77a73ea41998466cd4e
Priyanga-Packirisamy <110887702+Priyanga-Packirisamy@users.noreply.github.com>
2023-02-23 14:52:01 +0530

    Delete StudentRepo.java

--- StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Entity.Student;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-
-}
